f089bb5855c0fedbe72fdcb55a6c9c8350c37eb3
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-10-03 19:30:12 +0200

    Update database_connector.R

--- database_connector.R
+++ database_connector.R
@@ -1,1 +1,15 @@
+install.packages("RSQLite")
+
+library(DBI)
+
+con <- dbConnect(RSQLite::SQLite(), ":memory:")
+
+res <- dbSendQuery(con, "CREATE TABLE table2 (ID integer, NAME varchar);")
+res <- dbSendQuery(con, "INSERT INTO table2 VALUES (1,'val1');")
+res <- dbSendQuery(con, "INSERT INTO table2 VALUES (2,'val2');")
+res <- dbSendQuery(con, "SELECT * FROM table2;")
+dbFetch(res)
+
+dbListTables(con)
+
 
