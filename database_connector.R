f5e0271409f56a5a493182a2a6b5e39832a6651e
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-10-03 19:58:40 +0200

    Update database_connector.R

--- database_connector.R
+++ database_connector.R
@@ -1,14 +1,34 @@
-install.packages("RSQLite")
+#install.packages("RSQLite")
+
+options(stringsAsFactors=FALSE)
 
 library(DBI)
 
 con <- dbConnect(RSQLite::SQLite(), ":memory:")
 
-res <- dbSendQuery(con, "CREATE TABLE table2 (ID integer, NAME varchar);")
-res <- dbSendQuery(con, "INSERT INTO table2 VALUES (1,'val1');")
-res <- dbSendQuery(con, "INSERT INTO table2 VALUES (2,'val2');")
-res <- dbSendQuery(con, "SELECT * FROM table2;")
-dbFetch(res)
+data=read.csv("data.txt",sep="\t",header=T)
+print(data)
+print(cn)
+cmd="CREATE TABLE table1 (";
+cn=colnames(data)
+for(x in 1:length(cn)){
+	print(cn[x])
+	cmd=paste0(cmd,cn[x])
+	cmd=paste0(cmd," varchar")
+	if(x!=(length(cn))){
+		cmd=paste0(cmd,",")
+	}
+}
+cmd=paste0(cmd,");")
+print(cmd)
+
+# Creating a SQLite table
+res <- dbSendQuery(con, cmd)
+
+#res <- dbSendQuery(con, "INSERT INTO table2 VALUES (1,'val1');")
+#res <- dbSendQuery(con, "INSERT INTO table2 VALUES (2,'val2');")
+#res <- dbSendQuery(con, "SELECT * FROM table2;")
+#print(dbFetch(res))
 
 dbListTables(con)
 
